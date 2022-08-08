74256c08fb86e582f9d08e67f4ca7caaa40f8830
AramosA0 <lm20101a@gmail.com>
2022-08-08 12:45:09 -0500

    avance usuarios y chat

--- public/src/pages/Chat/index.jsx
+++ public/src/pages/Chat/index.jsx
@@ -1,9 +1,69 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
+import { AllUseresRoutes } from "../../util/ApiRoutes";
+
 const Chat = () => {
+
+    const navigate = useNavigate();
+    const [contact, setContact] = useState([]);
+    const [currentUser, setCurrentUser] = useState(undefined);
+    
+
+
+
+    
+    useEffect(()=>{
+        const getContact = async () => {
+            if(!localStorage.getItem("chat-app-user")){
+                navigate("/login")
+            }else {
+                setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+            }
+        }
+        getContact();
+        const getContactList = async () => {
+            if(currentUser){
+                if(currentUser.isAvatarImageSet){
+                    const data = await axios.get(`${AllUseresRoutes}/${currentUser.id}`);
+                    setContact(data.data);
+                }else {
+                    navigate("/setAvatar");
+                }
+            }
+        }
+        getContactList();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
     return (
-        <div>
-        <h1>Chat</h1>
-        </div>
+        <>
+            <Container>
+                <div className="container"></div>
+            </Container>
+        </>
     );
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .container {
+        height: 85vh;
+        width: 85vw;
+        background-color: #00000076;
+        display: grid;
+        grid-template-columns: 25% 75%;
+        @media screen and (min-width: 720px) and (max-width: 1024px) {
+            grid-template-columns: 35% 65%;
+        }
+`;
 
 export default Chat;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,54 +1,87 @@
-const User = require('../model/userModel');
-const brcypt = require('bcrypt');
+const User = require("../model/userModel");
+const brcypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
-    try {
-        const {username,email,password} = req.body;
-        const userNameCheck = await User.findOne({ username});
-        if(userNameCheck){
-            return res.status(400).json({
-                message: "User name already exists",
-                status: false
-            });
-        }
-        const emailCheck = await User.findOne({ email});
-        if(emailCheck){
-            return res.status(400).json({
-                message: "Email already exists",
-                status: false
-            });
-        }
-        const hashedPassword = await brcypt.hash(password, 10);
-        const user = await User.create({
-            email, username, password:hashedPassword
-        });
-        delete user.password;
-        return res.json({status: true, user});
-    } catch (error) {
-        next(error);
+  try {
+    const { username, email, password } = req.body;
+    const userNameCheck = await User.findOne({ username });
+    if (userNameCheck) {
+      return res.status(400).json({
+        message: "User name already exists",
+        status: false,
+      });
     }
-}
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.status(400).json({
+        message: "Email already exists",
+        status: false,
+      });
+    }
+    const hashedPassword = await brcypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports.login = async (req, res, next) => {
-    try {
-        const {username,password} = req.body;
-        const user = await User.findOne({ username});
-        if(!user){
-            return res.json({
-                message: "Incorrect username or password",
-                status: false
-            });
-        }
-        const passwordCheck = await brcypt.compare(password, user.password);
-        if(!passwordCheck){
-            return res.json({
-                message: "Incorrect username or password",
-                status: false
-            });
-        }
-        delete user.password;
-        return res.json({status: true, user});
-    } catch (error) {
-        next(error);
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({
+        message: "Incorrect username or password",
+        status: false,
+      });
     }
-}
+    const passwordCheck = await brcypt.compare(password, user.password);
+    if (!passwordCheck) {
+      return res.json({
+        message: "Incorrect username or password",
+        status: false,
+      });
+    }
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.allUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params._id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json({ status: true, users });
+  } catch (error) {
+    next(error);
+  }
+};
